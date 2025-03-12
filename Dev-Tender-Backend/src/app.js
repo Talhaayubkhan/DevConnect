2dd3b8bf6871d2326bfdb78255d5309c9d4f9dda
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2025-03-12 16:07:45 +0500

    add user routes and update main route in app.js

--- Dev-Tender-Backend/src/app.js
+++ Dev-Tender-Backend/src/app.js
@@ -2,9 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  console.log("server request = ", req);
-  res.send("Hello from the server");
+app.use("/test", (req, res, next) => {
+  res.send("Main Route");
+  next();
+});
+app.get("/user", (req, res) => {
+  // res.send(req.body);
+  res.send({ firstName: "John", lastName: "Doe" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("User created successfully");
 });
 
 app.listen(5000, () => {
