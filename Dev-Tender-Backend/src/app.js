ef16abbea4c87f9cc0b1167e797e56e75144d883
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2025-03-13 21:42:38 +0500

    refactor app.js to improve route handling and logging for user requests

--- Dev-Tender-Backend/src/app.js
+++ Dev-Tender-Backend/src/app.js
@@ -2,17 +2,48 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res, next) => {
-  res.send("Main Route");
+app.use(
+  "/",
+  (req, res, next) => {
+    console.log("Starting express");
+    // res.send("Hello, World! 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling GET request for /user");
+    // res.send("User 1 details");
+    next();
+  }
+);
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling GET request for /user");
+    res.send("User 1 details");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling GET request for /user");
+    res.send("User 2 details");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling GET request for /user");
+    res.send("User 3 details");
+    next();
+  }
+);
+app.get("/hello", (req, res, next) => {
+  console.log("Handling GET request for /user");
+  res.send("User 1 details");
   next();
 });
-app.get("/user", (req, res) => {
-  // res.send(req.body);
-  res.send({ firstName: "John", lastName: "Doe" });
-});
 
-app.post("/user", (req, res) => {
-  res.send("User created successfully");
+app.use("/user", (req, res, next) => {
+  console.log("Ending express");
+  res.send("Hello, World! 3");
+  // next();
 });
 
 app.listen(5000, () => {
