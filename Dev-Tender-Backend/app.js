8fa4a6d935d7cb09ffce504eabe772a9a3e43bbd
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2025-03-21 20:52:37 +0500

    add user retrieval and feed routes; update user model to include id field

--- Dev-Tender-Backend/app.js
+++ Dev-Tender-Backend/app.js
@@ -4,13 +4,10 @@ const connectDB = require("./src/config/database");
 const app = express();
 const User = require("./src/models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Khan",
-    lastName: "Hello",
-    emailId: "khan@example.com",
-    passsword: "khan@123",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User Created Successfully");
@@ -18,6 +15,47 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("User Error");
   }
 });
+
+app.get("/users", async (req, res) => {
+  // const userEmail = req.body.emailId;
+  const userId = req.body.id;
+
+  try {
+    const findId = await User.findById(userId);
+    if (!findId) {
+      res.status(404).send("User Not Found with id: " + userId);
+    } else {
+      res.send(findId);
+    }
+  } catch (error) {
+    res.status(400).send("User Error");
+  }
+
+  // try {
+  //   console.log(userEmail);
+  //   const users = await User.find({ emailId: userEmail });
+  //   if (!users) {
+  //     res.status(404).send("User Not Found");
+  //   } else {
+  //     res.send(users);
+  //   }
+  // } catch (error) {
+  //   res.status(400).send("User Error");
+  // }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.status(404).send("No Users Found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("User Error");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("MongoDB Connected Successfully To Server");
--- Dev-Tender-Backend/src/models/user.js
+++ Dev-Tender-Backend/src/models/user.js
@@ -16,6 +16,9 @@ const userSchema = new mongoose.Schema({
   age: {
     type: Number,
   },
+  id: {
+    type: String,
+  },
   gender: {
     type: String,
   },
